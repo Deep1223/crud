d9a3c76ad8d52973e383c0e9b14e32c0caa5821e
Deep <deepdungarani2001@gmail.com>
2024-05-14 16:37:08 +0530

    Your commit message

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import appcss from './components/app.module.css';
+import Form from './components/form';
+import Viewform from './components/viewform';
+import Addform from './components/addform';
+import Editform from './components/editform';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2 className='App'>Crud Operation using Local Storage</h2>
+    
+      <div className={appcss.formdiv}>
+      <Router>
+          <Routes>
+            <Route path="/" element={<Form />} />
+            <Route path="/form" element={<Form />} />
+            <Route path="/Viewform" element={<Viewform />} />
+            <Route path="/addform" element={<Addform />} />
+            <Route path="/editform" element={<Editform />} />
+          </Routes>
+      </Router>
+      </div>
+
+      
+
     </div>
   );
 }
